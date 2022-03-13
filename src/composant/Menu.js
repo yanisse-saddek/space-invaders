af7251afe1f29c2264ded384d4c84d5bc94061c3
yanisse-saddek <yanisse321@gmail.com>
2022-03-13 15:37:57 +0100

    update

--- src/composant/Menu.js
+++ src/composant/Menu.js
@@ -3,14 +3,12 @@ import React from 'react'
 export default class App extends React.Component{
    constructor(props){
     super(props)
-     this.state={
-                 
-     }
 }
     render(){    
         return(
             <div>
-            <p onClick={()=>{this.props.start()}}>menu</p> 
+            <h1>Menu</h1>
+            <p onClick={()=>{this.props.start()}}>commencer</p> 
             {localStorage.getItem('score')?<p>meilleure score : {localStorage.getItem('score')}</p>:null}
             </div>
     )
